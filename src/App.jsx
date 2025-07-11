fa5d18c033e1c99a1b872f07707f3a23b873c649
MichaelFetene <michaelfetene402@gmail.com>
2025-07-11 04:24:42 -0700

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home';
+import Banner from './components/Banner/Banner';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-  <Home/>
+      <Home />
+    
     </div>
   )
 }
